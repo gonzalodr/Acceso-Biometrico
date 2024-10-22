912515f5c1675309f219ba5019040b731ab0d927
Stev3n831 <yeiler8310@gmail.com>
2024-10-22 09:11:05 -0600

    Cambios crud usuario

--- src/services/usuarioService.py
+++ src/services/usuarioService.py
@@ -1,19 +1,19 @@
 import bcrypt 
 from models.usuario import Usuario #llamando modulo `models` y a un archivo `usuario` para importar la clase `Usuario`
 
-#funcion privada: se inicia con un guion bajo
+
 def _contrasenaHash(contrasena):
     contrasena = str(contrasena)
     if len(contrasena) < 8 :
-        return False, f"¡La contraseña debe ser minimo de 8 caracteres!" #retorna dos variables
+        return False, f"¡La contraseña debe ser minimo de 8 caracteres!" 
     elif not contrasena.strip() or contrasena.find(' ') != -1:
         return False, "¡La contraseña no debe llevar espacios!"
     else:
         contrasena = contrasena.encode("utf-8")
         contrasena_hash = bcrypt.hashpw(contrasena,bcrypt.gensalt())
-        return True, contrasena_hash #retorna dos variables
+        return True, contrasena_hash 
     
-## Verificacion de contraseña
+
 def _verificarContrasenaHash(contrasena, contrasena_hash):
     contrasena = str(contrasena)
     contrasena = contrasena.encode("utf-8")
@@ -23,12 +23,51 @@ def _verificarContrasenaHash(contrasena, contrasena_hash):
         return False, "¡El usuario o contraseña es incorrecta!"    
 
 def insertarUsuario(usuario: Usuario):
-    exito, resultado = _contrasenaHash(usuario.contrasena) #recupera las dos variables
+    exito, resultado = _contrasenaHash(usuario.contrasena) 
     if not exito:
         return exito,resultado
     else:
         usuario.contrasena = resultado.decode("utf-8")
         return exito, f"¡Usuario [{usuario.usuario}], Contraseña [{usuario.contrasena}]!"
     
-def inicioSesion():
-    pass
+    def insertarUsuario(self, usuario: Usuario):
+        result = self._contrasenaHash(usuario.contrasena)
+        if not result["success"]:
+            return result
+        
+        usuario.contrasena = result["contrasena_hash"].decode("utf-8") 
+
+
+        result = self._validarCorreo(usuario.correo)
+        if not result["success"]:
+            return result
+
+        return self.usuarioData.create_usuario(usuario)
+    
+    def modificarUsuario(self, usuario: Usuario):
+
+        result = self._validarCorreo(usuario.correo)
+        if not result["success"]:
+            return result
+        
+        # Modificar usuario
+        return self.usuarioData.update_usuario(usuario)
+    
+    def eliminarUsuario(self, id_usuario):
+        return self.usuarioData.delete_usuario(id_usuario)
+
+    def obtenerListaUsuarios(self, pagina=1, tam_pagina=10, ordenar_por="Id", tipo_orden="ASC", busqueda=None):
+        return self.usuarioData.list_usuarios(pagina, tam_pagina, ordenar_por, tipo_orden, busqueda)
+
+    def obtenerUsuarioPorId(self, id_usuario):
+        return self.usuarioData.get_usuario_by_id(id_usuario)
+
+
+
+    def iniciar_sesion(self, correo, contrasena):
+
+        if not self._validar_correo(correo):
+            return {"success": False, "message": "El formato del correo no es válido"}
+
+        resultado = self.usuarioData.verificar_usuario_contrasena(correo, contrasena)
+        return resultado
