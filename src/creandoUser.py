1b1a3f804bac6303673aeeebf20c7525db2e814f
gonzalodr <129683931+gonzalodr@users.noreply.github.com>
2024-11-10 10:18:15 -0600

    cambios

--- src/creandoUser.py
+++ src/creandoUser.py
@@ -3,18 +3,19 @@ from services.personaService import *
 from models.usuario import *
 from services.usuarioService import *
 
-## crean primero una presona
-##Pservices = PersonaServices()
-##persona:Persona = Persona("Yeiler","Madrigal", "Matamoros","2003-02-05","12312353434","Soltero\\a","yeiler@gmail.com","Puerto viejo")
+if __name__ == "__main__":
+    ## crean primero una presona
+    Pservices = PersonaServices()
+    persona:Persona = Persona("Gonzalo","Dormos", "Rodriguez","2002-07-01","605310603","Soltero\\a","gonzalodormos26@gmail.com","Puerto viejo,Sarapiqui")
 
-## lo insertan 
-##Pservices.insertarPersona(persona)
+    ## lo insertan 
+    print(Pservices.insertarPersona(persona))
 
-## commentan lo anterior lo del insertpersona
-## se fija que id tiene el registro persona que hicieron y crean el usuario con ese id
-Uservices = UsuarioServices()
-#usuario contraseña e id de la persona
-user:Usuario = Usuario("yeiler","12345678",4)
-#user: Usuario = Usuario("Maik", "Maik0024", 1)
-Uservices.insertarUsuario(user)
+    ## commentan lo anterior lo del insertpersona
+    ## se fija que id tiene el registro persona que hicieron y crean el usuario con ese id
+    Uservices = UsuarioServices()
+    #usuario contraseña e id de la persona
+    user:Usuario = Usuario("gonzalo","12345678",1)
+    #user: Usuario = Usuario("Maik", "Maik0024", 1)
+    print(Uservices.insertarUsuario(user))
 
