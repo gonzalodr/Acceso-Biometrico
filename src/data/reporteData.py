965215ed7ea0771fbe8ef54a8d6b847b4138add9
javierdi12 <darlendiaz1212@gmail.com>
2025-02-28 18:05:46 -0600

    ya casi

--- src/data/reporteData.py
+++ src/data/reporteData.py
@@ -34,3 +34,56 @@ class ReporteData:
             if conexion:
                 conexion.close()
         return resultado 
+    
+    def update_perfil(self, reporte: Reporte):
+        conexion, resultado = conection()
+        if not resultado["success"]:
+            return resultado
+        
+        try:
+            cursor = conexion.cursor()
+            query = f"""UPDATE {TBREPORTE} SET
+            {TBREPORTE_ID_EMPLEADO} = %s,
+            {TBREPORTE_FECHA_GENERACION} = %s,
+            {TBREPORTE_TIPO_REPORTE} = %s,
+            {TBREPORTE_CONTENIDO} = %s
+            WHERE {TBREPORTE_ID} = %s"""
+            
+            cursor.execute(query, (
+                reporte.id_empleado,
+                reporte.fecha_generacion,
+                reporte.tipo_reporte,
+                reporte.contenido,
+                reporte.id
+            ))
+            conexion.commit()
+            resultado["success"] = True
+            resultado["message"] = "Reporte actualizado exitosamente."
+        except Exception as e:
+            resultado["success"] = False
+            resultado["message"] = f"Error al actualizar reporte: {e}"
+        finally:
+            if conexion:
+                conexion.close()
+        return resultado
+    
+    def delete_reporte(self, id):
+        conexion, resultado = conection()
+        if not resultado["success"]:
+            return resultado
+        
+        try:
+            cursor = conexion.cursor()
+            query = f"DELETE FROM {TBREPORTE} WHERE {TBREPORTE_ID} = %s"
+            cursor.execute(query, (id,))
+            conexion.commit()
+            
+            resultado["success"] = True
+            resultado["message"] = "Reporte eliminado exitosamente."
+        except Exception as e:
+            resultado["success"] = False
+            resultado["message"] = f"Error al eliminar reporte: {e}"
+        finally:
+            if conexion:
+                conexion.close()
+        return resultado
