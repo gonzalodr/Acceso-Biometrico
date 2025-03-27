b9569958c22f4c681a74a1662dd1bc33b45efbdc
St3v3n831 <yeiler8310@gmail.com>
2025-03-26 20:21:44 -0600

    26/3/2025
    Se termino con el diseño de la UI, también se reconstruyó el método de data de list_usuarios funcionando correctamente para que pueda usarse en la UI más adelante. También se le implemento un icono para acceder a la parte de usuarios y se creo su acceso en la ventana principal la cual esa parte ahorita esta comentada ya que esta pendiente realizar el método de cargar tabla para finalizar con la tarea de mostrar usuarios y la vista de la tabla en la UI

--- src/UI/AdministrarUsuario/adminUsuario.py
+++ src/UI/AdministrarUsuario/adminUsuario.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import *
 from PySide6.QtCore import *
 from Utils.Utils import *
 from services.usuarioService import *
+from functools import partial
 
 class AdminUsuario(QWidget):
     cerrar_adminU = Signal()
@@ -53,14 +54,12 @@ class AdminUsuario(QWidget):
         self.btnBuscar = QPushButton(text="Buscar")
         self.btnBuscar.setCursor(Qt.PointingHandCursor)
         self.btnBuscar.setFixedSize(minimoTamBtn)
-        self.btnBuscar.clicked.connect(self._buscarUsuario)
         Sombrear(self.btnBuscar, 20, 0, 0)
 
         self.btnCrear = QPushButton(text="Crear")
         self.btnCrear.setCursor(Qt.PointingHandCursor)
         self.btnCrear.setFixedSize(minimoTamBtn)
         self.btnCrear.setObjectName("crear")
-        self.btnCrear.clicked.connect(self._crear_Usuario)
         Sombrear(self.btnCrear, 20, 0, 0)
 
         layoutTop.addSpacing(25)
@@ -98,3 +97,96 @@ class AdminUsuario(QWidget):
         layoutButtom.setAlignment(Qt.AlignCenter)
         layoutButtom.setContentsMargins(10, 10, 10, 40)
         layoutButtom.setSpacing(5)
+
+        self.btnPrimerPagina = QPushButton(text="Primera Página")
+        self.btnPrimerPagina.setFixedSize(minimoTamBtn)
+        self.btnPrimerPagina.setEnabled(False)
+        self.btnPrimerPagina.clicked.connect(self._irPrimeraPagina)
+        Sombrear(self.btnPrimerPagina, 20, 0, 0)
+        
+        self.btnAnterior = QPushButton(text="Anterior")
+        self.btnAnterior.setEnabled(False)
+        self.btnAnterior.setFixedSize(minimoTamBtn)
+        self.btnAnterior.clicked.connect(self._irAnteriorPagina)
+        Sombrear(self.btnAnterior, 20, 0, 0)
+        
+        self.lblNumPagina = QLabel(text="Página 0 de 0 páginas")
+        self.lblNumPagina.setFixedSize(QSize(170, 40))
+        self.lblNumPagina.setAlignment(Qt.AlignCenter)
+        
+        self.btnSiguiente = QPushButton(text="Siguiente")
+        self.btnSiguiente.setEnabled(False)
+        self.btnSiguiente.setFixedSize(minimoTamBtn)
+        self.btnSiguiente.clicked.connect(self._irSiguientePagina)
+        Sombrear(self.btnSiguiente, 20, 0, 0)
+        
+        self.btnUltimaPagina = QPushButton(text="Última Página")
+        self.btnUltimaPagina.setEnabled(False)
+        self.btnUltimaPagina.setFixedSize(minimoTamBtn)
+        self.btnUltimaPagina.clicked.connect(self._irUltimaPagina)
+        Sombrear(self.btnUltimaPagina, 20, 0, 0)
+        
+        layoutButtom.addWidget(self.btnPrimerPagina)
+        layoutButtom.addSpacing(10)
+        layoutButtom.addWidget(self.btnAnterior)
+        layoutButtom.addSpacing(10)
+        layoutButtom.addWidget(self.lblNumPagina)
+        layoutButtom.addSpacing(10)
+        layoutButtom.addWidget(self.btnSiguiente)
+        layoutButtom.addSpacing(10)
+        layoutButtom.addWidget(self.btnUltimaPagina)
+
+        self.layoutFrame.addLayout(layoutButtom)
+        frame.setLayout(self.layoutFrame)
+        layout.addWidget(frame)
+        self.setLayout(layout)
+        Sombrear(self, 30, 0, 0)
+        self._cargar_tabla()
+
+    def _cerrar(self):
+        self.cerrar_adminU.emit()
+
+    def _actualizar_lblPagina(self, numPagina, totalPagina):
+        self.lblNumPagina.setText(f"Página {numPagina} de {totalPagina}")
+
+    def _actualizarValoresPaginado(self, paginaActual, totalPaginas):
+        self.paginaActual = paginaActual
+        self.ultimaPagina = totalPaginas
+        if totalPaginas <= 1:
+            self.btnPrimerPagina.setEnabled(False)
+            self.btnAnterior.setEnabled(False)
+            self.btnSiguiente.setEnabled(False)
+            self.btnUltimaPagina.setEnabled(False)
+        elif paginaActual == 1:
+            self.btnPrimerPagina.setEnabled(False)
+            self.btnAnterior.setEnabled(False)
+            self.btnSiguiente.setEnabled(True)
+            self.btnUltimaPagina.setEnabled(True)
+        elif paginaActual == totalPaginas:
+            self.btnPrimerPagina.setEnabled(True)
+            self.btnAnterior.setEnabled(True)
+            self.btnSiguiente.setEnabled(False)
+            self.btnUltimaPagina.setEnabled(False)
+        else:
+            self.btnPrimerPagina.setEnabled(True)
+            self.btnAnterior.setEnabled(True)
+            self.btnSiguiente.setEnabled(True)
+            self.btnUltimaPagina.setEnabled(True)
+
+    def _irPrimeraPagina(self):
+        self.paginaActual = 1
+        self._cargar_tabla()
+
+    def _irUltimaPagina(self):
+        self.paginaActual = self.ultimaPagina
+        self._cargar_tabla()
+
+    def _irSiguientePagina(self):
+        if (self.paginaActual + 1) <= self.ultimaPagina:
+            self.paginaActual += 1
+            self._cargar_tabla()
+
+    def _irAnteriorPagina(self):
+        if (self.paginaActual - 1) >= 1:
+            self.paginaActual -= 1
+            self._cargar_tabla()
