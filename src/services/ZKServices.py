26829e71e1b5237ba507f0ee06a952425acaab5e
gonzalodr <129683931+gonzalodr@users.noreply.github.com>
2025-04-04 15:31:23 -0600

    04-04-2025 (Todos)
    agregando crud

--- src/services/ZKServices.py
+++ src/services/ZKServices.py
@@ -1,119 +1,94 @@
 import time
 from settings.config import ZKTECA_CONFIG
 from settings.logger import logger
-from zk              import ZK
+from zk              import ZK, const
+from datetime        import date,time, datetime
+from typing import Optional, List, Union, Tuple
+from Utils.Utils     import parse_date, parse_time
+import re
 
 class ZKServices:
     def __init__(self):
         self.zk = ZK(ZKTECA_CONFIG['host'],ZKTECA_CONFIG['port'])
-
-    def capture_fingerprint(self,conn):
-        """Función para capturar y obtener la huella dactilar."""
-        print('Por favor, coloca tu dedo en el lector...')
-        
-        # Espera a que se capture la huella
-        while True:
-            try:
-                fingerprint = conn.capture_finger()
-                print('Huella capturada con éxito.')
-                return fingerprint  # Devuelve la huella capturada
-            except Exception as e:
-                print(f'Error al capturar la huella: {e}')
-                time.sleep(1)  # Espera un segundo antes de volver a intentar
-
-    def crear_usuario(self,id:int, nombre:str):
-        try:
-            conn = self.zk.connect()
-            conn.set_user(id,nombre)
-            conn.disconnect()
-        except Exception as e:
-            logger.error(f'{e}')
-    
-
-    # registra 
-    def registrar_Empleado(self, id_empleado, nombre, cedula):
-        try:
-            conexion = self.zk.connect()
-            conexion.set_user(id_empleado,f"{nombre} {cedula}")
-            listaUser = conexion.get_users()
-            user = list(filter(lambda u:u.uid == id_empleado,listaUser))
-            conexion.disconnect()
-            return user
-        except Exception as e:
-            return f"Error {e}"
     
     # obteniendo huella
     def obtener_huella(self, id_empleado):
         try:
             conexion = self.zk.connect()
-            listFingers = conexion.get_templates()
-            finger = list(filter(lambda f: f.uid == id_empleado, listFingers)) or None
+            listHuella = conexion.get_templates()
+            huella = list(filter(lambda f: f.uid == id_empleado, listHuella)) or None
             conexion.disconnect()
-            return finger
+            return {'success':True, 'huella': huella[0] if huella else huella}
         except Exception as e:
-            return f'Error{e}'
-        
-        
-    def mostrar_ventana_huella(self,id_empleado):
+            return {'success':False, 'message':'Ocurrio un error al obtener la huella'}
 
-        try:
-            conn = self.zk.connect()
-            conn.enable_device()
-            
-            exito = conn.enroll_user(user_id=id_empleado, temp_id=1)
-
-            start_time = time.time()
-            while (time.time() - start_time) < 30:
-                templates = conn.get_templates()
-                huella    = list(filter(lambda t: t.uid == str(id_empleado), templates)) or None
-                print(huella)
-                if huella: 
-                    print("Huella recuperada..")
-                    print(f"id huella {huella.uid}")
-                    return "Huella compleatada"
-                    break
-                else:
-                    print('No hay huella')
-                time.sleep(1)
-            
-            conn.disconnect()
-            return None  # Timeout
-        except Exception as e:
-            print(f"Error del dispositivo: {e}")
-            return False
-        
-    def registrar_empleado_con_huella(self, id_em:int, nombre:str, cedula:str):
+    #registrar empleado con huella
+    def registrar_empleado_con_huella(self, id_empleado:int, nombre:str, cedula:str):
         try:
             conn = self.zk.connect()
             conn.enable_device()
 
-            conn.set_user(uid=id_em,name=f'{nombre}-{cedula}')
+            conn.set_user(uid = id_empleado, name = f'{nombre}-{cedula}')
             time.sleep(1)
-            print('Ingrese la huella digital..')
 
-            print('-------------------------------------')
-
-            exito = conn.enroll_user(uid=id_em, temp_id=9)
-
-            print("valor de exito")
-            print(exito)
-
+            conn.enroll_user(uid=id_empleado, temp_id=9)
 
             start_time = time.time()
             while (time.time() - start_time) < 30:
                 templates = conn.get_templates()
-                huella    = list(filter(lambda t: t.uid == id_em, templates)) or None
-                print(huella)
+                huella    = list(filter(lambda t: t.uid == id_empleado, templates)) or None
                 if huella: 
-                    print("Huella recuperada..")
-                    print(f"id huella {huella.uid}")
-                    return "Huella compleatada"
-                    break
-                else:
-                    print('No hay huella')
-                time.sleep(1)
-            
-            print('-------------------------------------')
-            return "No se completo la tarea"
+                    conn.test_voice(0)
+                    return {'success':True,'message':'Se registro con exito el empleado.','huella_id_ZK':huella[0].uid}
+                time.sleep(0.5)
+
+            return {'success':True,'message':'Se registro el empleado pero sin huella.','huella_id_ZK':None}
         except Exception as e:
-            return f'{e}'
+            logger.error(f'{e}')
+            return {'success':False, 'message':'Ocurrio un error al registrar el empleado.'}
+    
+    def obtener_asistencias(self, id_empleado: Optional[int]    = None, 
+                        fecha: Optional[Union[date, str]]       = None,
+                        rango_fechas: Optional[Union[Tuple[date, date], List[str]]] = None):
+        try:
+            if isinstance(fecha, str):
+                fecha = parse_date(fecha)
+                
+            if isinstance(rango_fechas, list):
+                if len(rango_fechas) != 2:
+                    raise ValueError("El rango de fechas debe contener exactamente 2 elementos")
+                rango_fechas = (parse_date(rango_fechas[0]), parse_date(rango_fechas[1]))
+            
+            if rango_fechas[0] > rango_fechas[1]:
+                return {'success':False,'message':'la primera fecha del filtro debe ser menor o igual a la ultima fecha.'}
+
+            conn        = self.zk.connect() 
+            conn.disable_device()
+            
+            asistencias = conn.get_attendance()
+            filtered    = []
+
+            for asist in asistencias:
+                asist_datetime = asist.timestamp
+                
+                # Filtro por ID de empleado
+                if id_empleado and str(asist.user_id) != str(id_empleado):
+                    continue
+                # Filtro por fecha específica
+                if fecha and asist_datetime.date() != fecha:
+                    continue
+                    
+                # Filtro por rango de fechas
+                if rango_fechas:
+                    fecha_inicio, fecha_fin = rango_fechas
+                    if not (fecha_inicio <= asist_datetime.date() <= fecha_fin):
+                        continue
+                
+                filtered.append(asist)
+
+            conn.enable_device()
+            conn.disconnect()
+            return {'success':True, 'message':'Se obtuvieron las asistencias.','asistencias':filtered}
+        except Exception as e:
+            logger.error(f'{e}')
+            return {'success':False, 'message':'Ocurrio un erro al obtener la asistencias'}
