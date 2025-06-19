24e7d5ab459acf27ecb74ffe36246681f0eb1b03
St3v3n831 <yeiler8310@gmail.com>
2025-06-18 22:16:42 -0600

    Revert "prueba correo 3"
    This reverts commit 412a1855a72977cbd950fa7971d9c066c31e2c44.

--- src/data/SolicitudPermisoData.py
+++ src/data/SolicitudPermisoData.py
@@ -15,7 +15,7 @@ class SolicitudPermisoData:
             solicitud (SolicitudPermiso): Objeto con los datos de la solicitud
 
         Returns:
-            dict: Diccionario con el resultado de la operación d
+            dict: Diccionario con el resultado de la operación
         """
         conexion, resultado = conection()
         if not resultado["success"]:
